9d53ee7cd0d7dbf0b9da0612c226de81b0e4a488
AlexV <asmodehn@gmail.com>
2016-10-25 08:00:23 +0900

    Rebase from 'debian/jade/pyros_test'

--- nodes/slowService.py
+++ nodes/slowService.py
@@ -3,17 +3,22 @@
 import rospy
 from std_srvs.srv import Empty, EmptyResponse
 
+delay = 30
+
 
 def handle_msg(rq):
     print("Slow Node got request")
-    rospy.rostime.wallsleep(30)
+    rospy.rostime.wallsleep(delay)
     return EmptyResponse()
 
 
 def empty_server():
     rospy.init_node('slow_node')
+    rospy.set_param('~slow_param', delay)
     srv = rospy.Service('/test/slowsrv', Empty, handle_msg)
     rospy.spin()
+    if rospy.has_param('slow_param'):
+        rospy.delete_param('~slow_param')
 
 if __name__ == '__main__':
     empty_server()
