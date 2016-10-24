dc941867521c1e742fc702f9078a5ce8aa298d0c
AlexV <asmodehn@gmail.com>
2016-10-25 07:50:59 +0900

    Imported upstream version '0.0.5' of 'upstream'

--- nodes/triggerService.py
+++ nodes/triggerService.py
@@ -3,16 +3,21 @@
 import rospy
 from std_srvs.srv import Trigger, TriggerResponse
 
+confirm_msg = "trigger received"
+
 
 def handle_msg(rq):
     print("Trigger Node got request")
-    return TriggerResponse(success=True, message="trigger received")
+    return TriggerResponse(success=True, message=confirm_msg)
 
 
 def trigger_server():
     rospy.init_node('trigger_node')
+    rospy.set_param('/test/confirm_param', confirm_msg)
     srv = rospy.Service('/test/trgsrv', Trigger, handle_msg)
     rospy.spin()
+    if rospy.has_param('/test/confirm_param'):
+        rospy.delete_param('/test/confirm_param')
 
 
 if __name__ == '__main__':
