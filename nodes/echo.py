b2ed105dfec2e269188b0a833cfc3463290a6701
alexv <asmodehn@gmail.com>
2016-11-07 20:38:02 +0900

    Rebase from 'release/jade/pyros_test'

--- nodes/echo.py
+++ nodes/echo.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+import sys
 
 import rospy
 import pyros_test
@@ -10,8 +10,10 @@ import pyros_test
 
 
 if __name__ == '__main__':
-    rospy.init_node('pyros_test_echo')
+    args = rospy.myargv(argv=sys.argv)
+    node_name = args[1] if len(args) > 1 else 'pyros_test_echo'
+
+    rospy.init_node(node_name)
     node = pyros_test.EchoNode()
     node.spin()
-
 
